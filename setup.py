f4aa6937f9a451b2d6b97310d842181215ba0d53
SLikhachev <aughing@hotmail.com>
2021-05-05 17:25:39 +1000

    wheel infra added

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='barsxml',
+    version='0.1.1',
+    url='https://github.com/SLikhachev/barsxml',
+    author='SLikhachev',
+    author_email='polaughing@yahoo.com',
+    license='BSD2',
+    description='make BARS XML packet from SQL',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    #package_dir={"": "src"},
+    python_requires=">=3.6",
+    packages=find_packages(
+        include=['config', 'path_class', 'xml_class'],
+        exclude=['tests']
+    ),
+    install_requires=['pyodbc >= 4.0.18', 'psycopg2 >= 2.7.3.2'], 
+    zip_safe=False
+)
