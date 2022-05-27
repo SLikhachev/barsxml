ebbc26d6e52fae2eb3f6a3be106059b53b1d90e9
SLikhachev <aughingl@gmail.com>
2022-05-27 12:26:51 +1000

    pytest.ini added

--- src/barsxml/tests/test_pg_app.py
+++ src/barsxml/tests/test_pg_app.py
@@ -1,17 +1,29 @@
 """ test """
 
 import importlib
+from pathlib import Path
 from datetime import datetime
 from barsxml.xmlprod.barsxml import BarsXml
 
+pytest_plugins= ("pytest_env",)
 
 def test_app():
     """ test """
     time1 = datetime.now()
     config = importlib.import_module('barsxml.tests.xml_pg_config')
-    xml = BarsXml(config, 'app', '250796', '11', 1)
-    rc, pc, zname, errors = xml.make_xml(False, False, False)
-    log = f"APP rc={rc}  pc={pc}  zname={zname}, errors={errors}"
+
+    if not Path.exists(config.BASE_XML_DIR):
+        Path.mkdir(config.BASE_XML_DIR)
+
+    pack = config.BASE_XML_DIR / config.PACK
+
+    if not Path.exists(pack):
+        Path.mkdir(pack)
+
+    xml = BarsXml(config, config.PACK, config.MO_CODE, config.MONTH, 1)
+    _rc, _pc, zname, errors = xml.make_xml(False, False, False)
+
+    log = f"APP rc={_rc}  pc={_pc}  zname={zname}, errors={errors}"
     print(log)
     time2 = datetime.now()
-    print(' Processing time %s sec' % (time2 - time1).seconds)
+    print(f'Processing time {(time2-time1).seconds} sec')
--- src/barsxml/tests/xml_pg_config.py
+++ src/barsxml/tests/xml_pg_config.py
@@ -1,21 +1,26 @@
+""" test config """
 
+import os
 from barsxml.path.thispath import Path
 from barsxml.config.postgresxml import *
 
+
 SQL_SRV = dict(
-    # host='192.168.1.31',
-    host='127.0.0.1',
-    dbname='hokuto',
-    user='postgres',
-    password='boruh',
-    schema='public',
-    dbauth=None
+    port=os.getenv('DB_PORT') or 5432,
+    host=os.getenv('DB_HOST') or '127.0.0.1',
+    dbname = os.getenv('DB_NAME') or 'hokuto',
+    user=os.getenv('DB_USER') or 'postgres',
+    password=os.getenv('DB_PASSWORD') or 'boruh',
+    schema=os.getenv('DB_SCHEMA') or 'public',
+    dbauth=os.getenv('DB_AUTH') or None
 )
 
 tests_dir = Path.script_dir()
 MO_CODE = "250796"
-YEAR = "2021"
-BASE_XML_DIR = tests_dir / 'data' / 'hokuto'
+PACK= os.getenv('TEST_PAC') or "app"
+YEAR = os.getenv('TEST_YEAR') or "2021"
+MONTH = os.getenv('TEST_MONTH') or "11"
+BASE_XML_DIR = tests_dir / 'data' / SQL_SRV['dbname']
 
 DS = dict(
     p_per=1,
