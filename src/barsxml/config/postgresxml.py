c25aa8ed883969f0c1779533f6b7c8dedbac26e0
SLikhachev <aughingl@gmail.com>
2026-03-05 13:53:08 +1000

    Исправлен косяк в вызове SQL функции

--- src/barsxml/config/postgresxml.py
+++ src/barsxml/config/postgresxml.py
@@ -35,7 +35,7 @@ MO_LOCAL = 'mo_local'
 
 
 #127.0.0.1:7000/rpc/get_hpm_data?tbl=talonz_clin_21&mont=4&fresh=0
-GET_HPM_DATA = 'SELECT * FROM get_hpm_data({talon_tbl}, {int_month}, {get_fresh})'
+GET_HPM_DATA = "SELECT * FROM get_hpm_data('{talon_tbl}', {int_month}, {get_fresh})"
 GET_ALL_LOCAL_MO = 'SELECT * FROM get_mo_local'
 GET_ALL_USL= 'SELECT * FROM get_pmu_usl(%s, %s, %s)'
 GET_SPEC_USL = 'SELECT * FROM get_spec_usl'
